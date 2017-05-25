0da1926ae515b548d4b736fe918bf2b115a9826b
StarkOne <Stark.pro40@gmail.com>
2017-05-25 12:58:07 +0700

    first

---
+++ app/js/common.js
@@ -0,0 +1,12 @@
+$(function() {
+
+  $('.owl-carousel').owlCarousel({
+        items:1,
+        loop:false,
+        margin:10,
+        URLhashListener:true,
+        autoplayHoverPause:true,
+        startPosition: 'URLHash'
+    });
+
+});
